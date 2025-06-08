8d30e6f74f9cf8478f92b74eeae4101fe1d3c55c
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-06-08 02:32:08 +0200

    Async await

--- 02-JS-review/starter/async.js
+++ 02-JS-review/starter/async.js
@@ -1,3 +1,15 @@
-fetch("https://jsonplaceholder.typicode.com/todos").then();
+/* fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json)
+  .then((data) => console.log(data));
 
 console.log("jonas");
+ */
+
+async function g() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+
+  console.log(data);
+}
+
+g();
