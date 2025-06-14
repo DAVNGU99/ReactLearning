142750894ce0a1063c76800a3273020f14af964f
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-06-15 01:30:22 +0200

    section 7 complete

--- tipcalculator/src/App.js
+++ tipcalculator/src/App.js
@@ -5,35 +5,57 @@ export default function App() {
 }
 
 function TipCalculator() {
-  const [total, setTotal] = useState(222);
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
 
   return (
     <div>
-      <Bill onSetTotal={setTotal} />
-      <Service>How did you like the service?</Service>
-      <Service>How did your friend like the service?</Service>
-      <Total total={total} />
+      <Bill onSetBill={setBill} bill={bill} />
+      <Service percentage={percentage1} onSetPercentage={setPercentage1}>
+        How did you like the service?
+      </Service>
+      <Service percentage={percentage2} onSetPercentage={setPercentage2}>
+        How did your friend like the service?
+      </Service>
+      <Total bill={bill} tip={tip} />
+      <Reset onReset={handleReset} />
     </div>
   );
 }
 
-function Bill({ onSetTotal }) {
+function Bill({ onSetBill, bill }) {
   return (
     <div>
       <h2>
         <label>How much was the bill?</label>
-        <input type="text"></input>
+        <input
+          value={bill}
+          onChange={(e) => onSetBill(Number(e.target.value))}
+          type="text"
+        ></input>
       </h2>
     </div>
   );
 }
 
-function Service({ children }) {
+function Service({ children, percentage, onSetPercentage }) {
   return (
     <div>
       <h2>
         <span>{children}</span>
-        <select>
+        <select
+          value={percentage}
+          onChange={(e) => onSetPercentage(Number(e.target.value))}
+        >
           <option value="0">Bad(0%)</option>
           <option value="5">Ok(5%)</option>
           <option value="10">Good(10%)</option>
@@ -44,12 +66,16 @@ function Service({ children }) {
   );
 }
 
-function Total({ total }) {
+function Total({ bill, tip }) {
   return (
     <div>
-      <h1>You pay {total}</h1>
+      <h1>
+        You pay {bill + tip} ({bill} + {tip})
+      </h1>
     </div>
   );
 }
 
-function Reset() {}
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
+}
