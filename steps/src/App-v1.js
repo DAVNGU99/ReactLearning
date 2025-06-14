ce9a1f7bff703773a2d8cdf1579c6b375be5558e
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-06-14 17:32:53 +0200

    children props

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -36,27 +36,21 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button bgColor="#7950F2" textColor="#fff" onClick={handlePrevious}>
-              <span>🤗</span> Previous
-            </Button>
-
-            <Button bgColor="#7950F2" textColor="#fff" onClick={handleNext}>
-              Next <span>😝</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950F2", color: "white" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950F2", color: "white" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}{" "}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
