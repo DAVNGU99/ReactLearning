3bc54cb6fbff2333b8081a5607c882effe31d219
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-06-16 23:33:07 +0200

    popcorn project

---
+++ pizzamenu/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
+}
+
+export default App;
