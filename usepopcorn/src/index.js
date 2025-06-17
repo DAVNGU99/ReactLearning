86a08b2d7de7cbe4229450426846a04878fb9408
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-06-17 19:56:43 +0200

    proptypes

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 /* import './index.css';
 import App from './App'; */
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie have {movieRating} stars </p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating size={24} color="#ff3211" defaultRating={4} />
+    <Test />
   </React.StrictMode>
 );
