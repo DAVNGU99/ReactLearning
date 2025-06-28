91341a516d11ff3a6be6c86c2a9033b14de1e9ea
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-06-28 21:06:16 +0200

    fe

--- react-quiz/src/components/Question.js
+++ react-quiz/src/components/Question.js
@@ -1,8 +1,11 @@
+import Options from "./Options";
+
 function Question({ question }) {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
+      <Options question={question} />
     </div>
   );
 }
