c694bdce6124691ea7232f56e82874cff85306a6
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-07-03 15:11:08 +0200

    useContext

--- worldwise/src/context/CitiesContext.jsx
+++ worldwise/src/context/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
@@ -35,4 +35,13 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+
+  if (context === undefined)
+    throw new Error("Cities context was used outside the Cities provider");
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
