e624f0edba8900fd61f397d69ae8f8336b4006ba
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-07-31 23:31:03 +0200

    more styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2 rem;
 `;
 
 function App() {
