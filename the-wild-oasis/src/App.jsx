446f92e50263200f7efa6dc0a43f41af5262249f
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-07-31 23:47:40 +0200

    more styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -8,14 +8,33 @@ const H1 = styled.h1`
 
 const Button = styled.button`
   font-size: 1.4rem;
-  padding: 1.2 rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
 `;
 
 function App() {
   return (
-    <div>
+    <StyledApp>
       <H1>The Wild Oasis</H1>
-    </div>
+      <Button>Check in</Button>
+      <Input type="number" placeholder="hello world" />
+    </StyledApp>
   );
 }
 
