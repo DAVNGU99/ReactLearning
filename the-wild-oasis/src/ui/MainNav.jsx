54b99a96a239f26e9967e3f30a6ed73e04d9bd71
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-09-15 21:32:56 +0200

    connected a backend to react app

--- the-wild-oasis/src/ui/MainNav.jsx
+++ the-wild-oasis/src/ui/MainNav.jsx
@@ -72,19 +72,19 @@ function MainNav() {
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/bookings">
+          <StyledNavLink to="/cabins">
             <HiOutlineHomeModern />
             <span>Cabins</span>
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/bookings">
+          <StyledNavLink to="/users">
             <HiOutlineUsers />
             <span>Users</span>
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/bookings">
+          <StyledNavLink to="/settings">
             <HiOutlineCog6Tooth />
             <span>Settings</span>
           </StyledNavLink>
