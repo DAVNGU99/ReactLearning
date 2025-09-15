82c96d0a17c7f7db263e4e476c25f61ce2960f09
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-09-15 21:41:13 +0200

    connected a backend to react app

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -16,6 +16,7 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://rpratdqfqgkcghxzbqyc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
     </Row>
   );
 }
